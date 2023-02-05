7f5fe6b195976f7749dd3f9a8b06105e490a72dc
Lucas Ortiz <lortiz@thehotelsnetwork.com>
2023-02-05 20:38:47 +0100

    chore: eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,17 +8,36 @@ module.exports = {
   extends: [
     'plugin:vue/vue3-recommended',
     'eslint:recommended',
-    '@vue/prettier',
     '@vue/typescript/recommended',
-    './eslintrc-auto-import.json',
+    './.eslintrc-auto-import.json',
   ],
   parserOptions: {
-    ecmaVersion: 2021,
+    parser: '@typescript-eslint/parser',
   },
   rules: {
-    'vue/multi-word-component-names': 'off',
-    'no-shadow': 'off',
     '@typescript-eslint/no-shadow': ['error'],
     '@typescript-eslint/no-unused-vars': ['error'],
+    semi: [2, 'always'],
+    indent: ['error', 2, { SwitchCase: 1 }],
+    'no-trailing-spaces': "error",
+    'no-shadow': ['error', { allow: ['state'] }],
+    'no-param-reassign': [2, { props: false }],
+    'vue/no-parsing-error': [2, { 'x-invalid-end-tag': false }],
+    'vue/no-v-for-template-key': 'off',
+    'vue/no-multiple-template-root': 'off',
+    'vue/valid-v-slot': 'off',
+    'vue/multi-word-component-names': 'off',
+    'vue/no-reserved-component-names': 'off',
+    'vue/no-v-text-v-html-on-component': 'off',
+    'vue/max-len': [
+      'error',
+      {
+        code: 120,
+        template: 140,
+        ignoreComments: true,
+        ignoreStrings: true,
+        ignoreTemplateLiterals: true,
+      },
+    ],
   },
-}
+};
