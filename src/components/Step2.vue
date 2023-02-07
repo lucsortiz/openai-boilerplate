29f0a65f15874cd0e6217aad2351522160626959
Lucas Ortiz <lortiz@thehotelsnetwork.com>
2023-02-07 17:35:10 +0100

    fix: styles

--- src/components/Step2.vue
+++ src/components/Step2.vue
@@ -8,7 +8,7 @@ const goToNext = () => {
     top: 0,
     behavior: 'smooth',
   });
-}
+};
 </script>
 
 <template>
@@ -23,96 +23,95 @@ const goToNext = () => {
       class="mx-auto"
     >
       <v-form
-        fast-fail
         v-model="valid"
+        fast-fail
         @submit.prevent="console.log('submit')"
       >
-            <v-container>
-                <v-row><h3>Social Media</h3></v-row>
-                <v-row>
-                    <v-col cols="4">
-                      <v-checkbox
-                          v-model="appState.socialMedia"
-                          label="Facebook"
-                          value="Facebook"
-                      ></v-checkbox>
-                      <v-checkbox
-                          v-model="appState.socialMedia"
-                          label="Instagram"
-                          value="Instagram"
-                      ></v-checkbox>
-                    </v-col>
-                    <v-col cols="4">
-                      <v-checkbox
-                          v-model="appState.socialMedia"
-                          label="TikTok"
-                          value="TikTok"
-                      ></v-checkbox>
-                      <v-checkbox
-                          v-model="appState.socialMedia"
-                          label="Linkedin"
-                          value="Linkedin"
-                      ></v-checkbox>
-                    </v-col>
-                    <v-col cols="4">
-                      <v-checkbox
-                          v-model="appState.socialMedia"
-                          label="Twitter"
-                          value="TikTok"
-                      ></v-checkbox>
-                    </v-col>
-                </v-row>
-                <v-row><h3>Marketing</h3></v-row>
-                <v-row>
-                  <v-col cols="6">
-                    <v-checkbox
-                        v-model="appState.marketingPlan"
-                        label="Marketing Plan"
-                        value="true"
-                    ></v-checkbox>
-                    <v-checkbox
-                        v-model="appState.email"
-                        label="Email"
-                        value="true"
-                    ></v-checkbox>
-                  </v-col>
-                  <v-col cols="6">
-                    <v-checkbox
-                      v-model="appState.newsletter"
-                      label="Newsletter"
-                      value="true"
-                  ></v-checkbox>
-                    <v-checkbox
-                        v-model="appState.slogan"
-                        label="Slogan"
-                        value="true"
-                    ></v-checkbox>
-                  </v-col>
-                  <v-col cols="12">
-                    <v-select
-                        v-model="appState.tone"
-                        label="Tone"
-                        :items="['Formal', 'Informal']"
-                    ></v-select>
-                  </v-col>
-                  <v-col cols="12">
-                    <v-textarea
-                        v-model="appState.objective"
-                        label="What is the main objective of your OUTPUT?"
-                        variant="outlined"
-                        :rules="queryTextRules"
-                    />
-                  </v-col>
-                </v-row>
-              <v-btn
-                  color="success"
-                  class="mt-4"
-                  block
-                  @click="goToNext"
-              >
-                Next
-              </v-btn>
-            </v-container>
+        <v-container>
+          <v-row><h3>Social Media</h3></v-row>
+          <v-row>
+            <v-col cols="4">
+              <v-checkbox
+                v-model="appState.socialMedia"
+                label="Facebook"
+                value="Facebook"
+              />
+              <v-checkbox
+                v-model="appState.socialMedia"
+                label="Instagram"
+                value="Instagram"
+              />
+            </v-col>
+            <v-col cols="4">
+              <v-checkbox
+                v-model="appState.socialMedia"
+                label="TikTok"
+                value="TikTok"
+              />
+              <v-checkbox
+                v-model="appState.socialMedia"
+                label="Linkedin"
+                value="Linkedin"
+              />
+            </v-col>
+            <v-col cols="4">
+              <v-checkbox
+                v-model="appState.socialMedia"
+                label="Twitter"
+                value="TikTok"
+              />
+            </v-col>
+          </v-row>
+          <v-row><h3>Marketing</h3></v-row>
+          <v-row>
+            <v-col cols="6">
+              <v-checkbox
+                v-model="appState.marketingPlan"
+                label="Marketing Plan"
+                value="true"
+              />
+              <v-checkbox
+                v-model="appState.email"
+                label="Email"
+                value="true"
+              />
+            </v-col>
+            <v-col cols="6">
+              <v-checkbox
+                v-model="appState.newsletter"
+                label="Newsletter"
+                value="true"
+              />
+              <v-checkbox
+                v-model="appState.slogan"
+                label="Slogan"
+                value="true"
+              />
+            </v-col>
+            <v-col cols="12">
+              <v-select
+                v-model="appState.tone"
+                label="Tone"
+                :items="['Formal', 'Informal']"
+              />
+            </v-col>
+            <v-col cols="12">
+              <v-textarea
+                v-model="appState.objective"
+                label="What is the main objective of your OUTPUT?"
+                variant="outlined"
+              />
+            </v-col>
+          </v-row>
+          <v-btn
+            color="success"
+            class="mt-4"
+            block
+            @click="goToNext"
+          >
+            Next
+          </v-btn>
+        </v-container>
       </v-form>
     </v-sheet>
   </v-card>
